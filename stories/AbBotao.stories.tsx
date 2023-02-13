28b4ebc7877fe452e37f34b747c3f447b3125188
varMatheus <108472426+varMatheus@users.noreply.github.com>
2023-02-13 09:48:14 -0300

    iniciando projeto de design system

---
+++ stories/AbBotao.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from "@storybook/react"
+import { AbBotao } from '../src';
+
+export default {
+  title: 'Componentes/AbBotao',
+  component: AbBotao
+} as ComponentMeta<typeof AbBotao>
+
+const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+
+export const Primario = Template.bind({});
